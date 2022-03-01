5dbf4dd440d22e2cdec62fdcf9fb4deb4019b8fb
xsummersx <605369747@qq.com>
2022-03-01 11:40:53 +0800

    pushTP

--- src/components/Main/Editor.vue
+++ src/components/Main/Editor.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2022-02-24 14:13:13
- * @LastEditTime: 2022-02-24 20:53:36
+ * @LastEditTime: 2022-03-01 11:21:58
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \lachart_js\src\components\Editor.vue
@@ -9,18 +9,19 @@
 <template>
   <VAceEditor
     v-model:value="content"
-    lang="javascript"
+    lang="json"
     theme="chrome"
     :options="editorOptions"
     :style="{ height: aceHeight }"
     style="width: 100%; border: 1px solid #eee"
+    ref="editorDOM"
   />
 </template>
 <script>
 import { VAceEditor } from "vue3-ace-editor";
 
 //引入主题
-import jsMode from "brace/mode/javascript";
+import "brace/mode/json";
 import "brace/theme/chrome";
 export default {
   name: "Editor",
@@ -39,16 +40,31 @@ export default {
         fontSize: 14,
         showPrintMargin: false, //去除编辑器里的竖线
       },
+      enpt: "",
     };
   },
   components: {
     VAceEditor,
   },
+  mounted() {
+    setTimeout(() => {
+      this.forMatter();
+    }, 200);
+  },
   methods: {
+    //延时格式化
+
     //格式化代码
     forMatter() {
-      this.content.getSession().setMode(new jsMode());
-      console.log(ok);
+      let cont = JSON.parse(this.content);
+      // console.log(cont);
+      //let contStr = JSON.stringify(this.content, null, 2);
+      // console.log(contStr);
+      // console.log("ok");
+      //this.$refs.editorDOM._editor.getSession().setTabSize(2);
+      // this.$refs.editorDOM._editor.getSession().setUseWrapMode(true);
+      //格式化JSON
+      this.$refs.editorDOM._editor.setValue(JSON.stringify(cont, null, "\t"));
     },
   },
 };
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,14 +1,13 @@
 <!--
  * @Author: your name
  * @Date: 2022-02-21 16:04:36
- * @LastEditTime: 2022-02-24 20:54:08
+ * @LastEditTime: 2022-03-01 11:36:53
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \lachart\src\components\Main\Main.vue
 -->
 <template>
   <Head class="headerShaw" />
-
   <div class="leftEdit">
     <div class="ItemBox">
       <el-button type="primary" class="btn" @click="BtnReload">刷新</el-button>
@@ -18,7 +17,13 @@
   </div>
   <div class="rightBox">
     <div class="ItemBox">
-      <el-tag class="ml-2 tag" type="danger">echarts：5.3.0</el-tag>
+      <el-button class="fl" style="margin-right: 12px" @click="saveBtn" type="success"
+        >保存</el-button
+      >
+      <el-tag class="ml-2 tag fr" type="danger">echarts：5.3.0</el-tag>
+      <el-button class="fl" type="primary" :plain="IsPlain" @click="CollectBtn" :icon="Star"
+        >收藏</el-button
+      >
     </div>
     <div class="chartBox">
       <div class="echartBox" style="width: 100%; height: 780px" id="Main"></div>
@@ -31,14 +36,16 @@ import { init } from "echarts";
 import { onMounted } from "vue";
 import Editor from "@/components/Main/Editor.vue";
 import Head from "@/components/Head/Head.vue";
+import { Star } from "@element-plus/icons-vue";
 //接口
 import { getChartDetail } from "@/api/getInfo.js";
 let content = ref("");
 //子组件的类
 const editor = ref(null);
 const aceHeight = ref("816px"); //框的高度
+let myChart;
 onMounted(() => {
-  let myChart = init(document.getElementById("Main"));
+  myChart = init(document.getElementById("Main"));
   getChartDetail().then((res) => {
     content.value = res.data.option;
     console.log(content.value);
@@ -49,14 +56,50 @@ onMounted(() => {
 });
 //刷新
 const BtnReload = () => {
-  console.log(editor);
-  let myChart = init(document.getElementById("Main"));
+  myChart = init(document.getElementById("Main"));
   let myStr = `myChart.setOption(${editor.value.content})`;
   eval(myStr);
 };
 //格式化代码
 const Format = () => {
   editor.value.forMatter();
+};
+//***收藏功能块***//
+let IsPlain = ref(false);
+const CollectBtn = (e) => {
+  IsPlain.value = !IsPlain.value;
+  //失去焦点
+  let target = e.target;
+  if (target.nodeName == "SPAN" || target.nodeName == "I") {
+    target = e.target.parentNode;
+  }
+  target.blur();
+};
+//**下载图片 */
+const saveBtn = () => {
+  // src.value = myChart.getDataURL({
+  //   pixelRatio: 2,
+  //   backgroundColor: "#fff",
+  // });
+  let offcan = myChart.getDataURL({
+    pixelRatio: 2,
+    backgroundColor: "#fff",
+  });
+  console.log(offcan);
+  const block = offcan.split(";");
+  const contentType = block[0].split(":")[1];
+  const realData = block[1].split(",")[1];
+  var blob = b64toBlob(realData, contentType);
+  const formData = new FormData();
+  formData.append("blob", blob);
+  // $.ajax({
+  //   url: url,
+  //   data: formData,
+  //   type: "POST",
+  //   async: true,
+  //   error: function (err) {},
+  //   success: function (data) {},
+  // });
 };
 </script>
 <style scoped lang="scss">
@@ -94,5 +137,8 @@ const Format = () => {
     height: 780px;
     padding: 10px;
   }
+}
+.fr {
+  float: right;
 }
 </style>
